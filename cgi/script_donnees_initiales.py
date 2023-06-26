75f5a3f84b298de7f225ca00cb55d81cb5fa45c9
Parox35 <antoine.esnault4@gmail.com>
2023-06-26 13:42:35 +0200

    script donnees init

--- cgi/script_donnees_initiales.py
+++ cgi/script_donnees_initiales.py
@@ -1,26 +1,65 @@
-import mysql.connector
 import sys
+import pandas as pd
+import os.path
 
+def prepaDB(df):
+    # split la colonne date en deux colonnes date et hour
+    df[['date', 'hour']] = df['date'].str.split(' ', expand=True)
 
-try:
-   conn = mysql.connector.connect(host="127.0.0.1", 
-                                  user="georges", password="what-else?", 
-                                  database="magasin")
-   cursor = conn.cursor()
+    # enleve les - dans la colonne date et les : dans la colonne hour
+    df['date'] = df['date'].str.replace('-', '')
+    df['hour'] = df['hour'].str.replace(':', '')
 
-   try:
-      
-      reference = (554871, "Confiture de fraise 250g", 10, 4.8) 
-      cursor.execute("""INSERT INTO Produits (latitude, longitude, stock, prix) VALUES(%s, %s, %s, %s)""", reference)
-      conn.commit()
-   except:
-      # En cas d'erreur on annule les modifications
-      conn.rollback()
-   
-except mysql.connector.errors.InterfaceError as e:
-   print("Error %d: %s" % (e.args[0],e.args[1]))
-   sys.exit(1)
-finally:
-   # On ferme la connexion
-   if conn:
-      conn.close()
+    # convertir la colonne date en int
+    df['date'] = df['date'].astype(int)
+    df['hour'] = df['hour'].astype(int)
+
+    return df
+
+def reducDim(df):
+    #Variable ne déterminant pas la gravité de l'accident
+    df_reduced = df.drop(columns=[ 'num_veh', 'id_code_insee', 'Num_Acc','id_usa', 'place', 'descr_agglo', 'descr_cat_veh', 'an_nais', 'descr_motif_traj', 'description_intersection', 'descr_type_col'])
+    
+    return df_reduced
+
+csv_file = sys.argv[1]
+file = pd.read_csv(csv_file, sep=';')
+df = prepaDB(file)
+df_reduced  = reducDim(df)
+list_df = df_reduced.values.tolist()
+print(list_df[1])
+
+file = open("data_init.sql", "w")
+if os.path.isfile("data_init.sql"):
+   file.write('-- Table Gravité')
+   file.write("DELETE FROM gravite;\n")
+   file.write("ALTER TABLE gravite\n AUTO_INCREMENT = 1;\n\n")
+   file.write('INSERT INTO gravite (descr_grav) VALUES \n("Indemne"),\n("Blessé léger"),\n("Blessé hospitalisé"),\n("Tué");")\n\n')
+
+   file.write('-- Table Etat de la surface')
+   file.write("DELETE FROM etat_surface;\n")
+   file.write("ALTER TABLE etat_surface\n AUTO_INCREMENT = 1;\n\n")
+   file.write('INSERT INTO etat_surface (descr_etat_surf) VALUES \n("Normale"),\n("Mouillée"),\n("Flaques"),\n("Inondée"),("Enneigée"),\n("Boue"),\n("Verglacée"),\n("Corps gras - huile"),\n ("Autre");")\n\n')
+
+   file.write('-- Table Conditions athmosphériques')
+   file.write("DELETE FROM conditions_atmospheriques;\n")
+   file.write("ALTER TABLE conditions_atmospheriques\n AUTO_INCREMENT = 1;\n\n")
+   file.write('INSERT INTO conditions_atmospheriques (descr_athmo) VALUES \n("Normale"),\n("Pluie légère"),\n("Pluie forte"),\n("Neige - grêle"),("Brouillard - fumée"),\n("Vent fort - tempête"),\n("Temps éblouissant"),\n("Temps couvert"),\n ("Autre");")\n\n')
+
+   file.write('-- Table Luminosité')
+   file.write("DELETE FROM luminosite;\n")
+   file.write("ALTER TABLE luminosite\n AUTO_INCREMENT = 1;\n\n")
+   file.write('INSERT INTO luminosite (descr_lum) VALUES \n("Plein jour"),\n("Crépuscule ou aube"),\n("Nuit sans éclairage public"),\n("Nuit avec éclairage public non allumé"),("Nuit avec éclairage public allumé");")\n\n')
+
+   file.write('-- Table Utilisation de dispositif de sécurité')
+   file.write("DELETE FROM securite;\n")
+   file.write("ALTER TABLE securite\n AUTO_INCREMENT = 1;\n\n")
+   file.write('INSERT INTO securite (descr_dispo_secu) VALUES \n("Utilisation d\'une ceinture de sécurité "),\n("Utilisation d\'un casque "),\n("Présence d\'une ceinture de sécurité - Utilisation non déterminable"),\n("Présence de ceinture de sécurité non utilisée "),\n("Autre - Non déterminable"),\n("Présence d\'un équipement réfléchissant non utilisé"),\n("Présence d\'un casque non utilisé "),\n("Utilisation d\'un dispositif enfant"),\n ("Présence d\'un casque - Utilisation non déterminable"),("Présence dispositif enfant - Utilisation non déterminable"),\n("Autre - Utilisé"), \n("Utilisation d\'un équipement réfléchissant "),\n("Autre - Non utilisé"), \n("Présence équipement réfléchissant - Utilisation non déterminable"),\n("Présence d\'un dispositif enfant non utilisé")\n;")\n\n')
+
+   file.write('INSERT INTO accident (age, date, heure, latitude, longitude, id_ville, id_lum, id_athmo, id_etat_surf, id_dispo_secu);\n')
+
+   file.write('(')
+   for list in list_df:
+      file.write('('+ str(list[7]) + ',' + str(list[0]) + ',' + str(list[10]) + ',' + str(list[2]) + ',' + str(list[3]) + ',' + str(list[1]) + ',' + str(list[5]) + ',' + str(list[4]) + ',' + str(list[6]) + ',' + str(list[8]) + ',' +'),\n')
+   file.write(');')
+file.close()
